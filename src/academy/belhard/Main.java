e991b80ec11db346f367a1547a4e3d0ce7c39494
asus <nik.kardymon@mail.ru>
2021-04-12 18:17:06 +0300

    Добавил демонстрацию работы

--- src/academy/belhard/Main.java
+++ src/academy/belhard/Main.java
@@ -1,10 +1,19 @@
 package academy.belhard;
 
+import academy.belhard.entity.Person;
+import academy.belhard.entity.User;
+
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
 
+        User uz1 = new User("Felix", "Bondarev", "Felix@gmail.com", "1234");
+        Person pe1 = new Person("Felix", "Bondarev");
+
+
+        System.out.println(uz1.getFullInfo());
+        System.out.println(uz1.isPasswordCorrect("1234"));
+        System.out.println(pe1.getFullName());
 
     }
 }
